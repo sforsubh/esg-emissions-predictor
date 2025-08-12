dffcdce08361a0a348f7876b434380a1b261b4cf
Subhadeep P <92154509+sforsubh@users.noreply.github.com>
2025-08-13 01:17:04 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write("Hello from Streamlit 🎈")
+
+st.set_page_config(page_title="Hello", page_icon="👋", layout="centered")
+st.title("Hello from Streamlit 🎈")
+st.write("If you can see this, the deploy pipeline is healthy.")
